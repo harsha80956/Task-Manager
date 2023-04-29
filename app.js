c94eac46dd6718c9cde407bb8630e666cb09f0e8
harsha80956 <harsha80956@gmail.com>
2023-04-29 10:06:02 +0530

    error handling for api's

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 // middlerware
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 8001;
+const port = process.env.PORT || 8001;
 
 const start = async () => {
   try {
